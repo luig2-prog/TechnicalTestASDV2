702d57062f28c97a6f5d4d4da4dc2adcd190a183
Luis Manuel Hernandez Jimenez <luis.hernandez@grupokonecta.com>
2023-08-19 12:22:45 -0500

    Initial commit

---
+++ src/main/java/com/asd/technicaltestasd/TechnicalTestAsdApplication.java
@@ -0,0 +1,13 @@
+package com.asd.technicaltestasd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalTestAsdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechnicalTestAsdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/asd/technicaltestasd/TechnicalTestAsdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asd.technicaltestasd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalTestAsdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
